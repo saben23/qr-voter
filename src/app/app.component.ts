a7fc0bcc81ab2220d3f6b175adcd1faba019c6d5
Sallér Bence <sabence@gmail.com>
2020-04-04 14:17:39 +0200

    Demo angular app added with PDFMake, ng-rating-bar and angular material

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'qr-voter';
+  rate = 3.56;
+
+  onRateChange(event: any): void {
+    console.log(event);
+  }
+
+  openPdf(): void {
+    const pdfContent = { content: [
+      // ...
+      {
+        text: 'Other Details',
+        style: 'header'
+      },
+      {
+        text: 'details'
+      },
+      {
+        text: 'Signature',
+        style: 'sign'
+      },
+      {
+        columns : [
+            { qr: 'http://192.168.0.133:4200', fit : 100 },
+            {
+            text: `(weburl)`,
+            alignment: 'right',
+            }
+        ]
+      }
+    ],
+      styles: {
+        // ...
+        sign: {
+          margin: [0, 50, 0, 10],
+          alignment: 'right',
+          italics: true
+        },
+      }
+    };
+    pdfMake.createPdf(pdfContent).open();
+  }
+}
