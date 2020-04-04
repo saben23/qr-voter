07c2405c7f9d23ba7dd4530160e77bfebba1892a
Sallér Bence <sabence@gmail.com>
2020-04-04 17:04:35 +0200

    FlexLayout and dark material custom theme added to the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
 
 @Component({
   selector: 'app-root',
@@ -11,45 +7,4 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 })
 export class AppComponent {
   title = 'qr-voter';
-  rate = 3.56;
-
-  onRateChange(event: any): void {
-    console.log(event);
-  }
-
-  openPdf(): void {
-    const pdfContent = { content: [
-      // ...
-      {
-        text: 'Other Details',
-        style: 'header'
-      },
-      {
-        text: 'details'
-      },
-      {
-        text: 'Signature',
-        style: 'sign'
-      },
-      {
-        columns : [
-            { qr: 'http://localhost:4200', fit : 100 },
-            {
-            text: `(weburl)`,
-            alignment: 'right',
-            }
-        ]
-      }
-    ],
-      styles: {
-        // ...
-        sign: {
-          margin: [0, 50, 0, 10],
-          alignment: 'right',
-          italics: true
-        },
-      }
-    };
-    pdfMake.createPdf(pdfContent).open();
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BarRatingModule } from 'ngx-bar-rating';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { VotingModule } from './features/voting/voting.module';
 
 @NgModule({
   declarations: [
@@ -10,8 +13,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BarRatingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    VotingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
